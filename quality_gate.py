1557c3a7aa8b0e51b50a267f8cabb558fe1e1b5b
D3aThNdDeMiSe <immadshah18@gmail.com>
2025-11-12 20:17:24 +0500

    Update to quality_gate.py
    Attempt to fix parameter error in quality_gate.py (threshold parameter)

--- quality_gate.py
+++ quality_gate.py
@@ -5,14 +5,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--threshold', type=float, required=True)
 args = parser.parse_args()
 
-with open(args.threshold, "r") as f:
-    eval_threshold = float(f.read().strip())
-
 with open("metrics/accuracy.json", "r") as f:
     metrics = json.load(f)
 accuracy = metrics["accuracy"]
 
-if accuracy < eval_threshold:
-    raise ValueError(f"Model accuracy {accuracy:.2f} is below the threshold of {eval_threshold:.2f}.")
+if accuracy < args.threshold:
+    raise ValueError(f"Model accuracy {accuracy:.2f} is below the threshold of {args.threshold:.2f}.")
 else:
     print(f"Model passed the quality gate with accuracy {accuracy:.2f}.")
